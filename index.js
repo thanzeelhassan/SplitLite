9a9a353ed2730d4afbbde9e5b76cc11164d79201
Thanzeel Hassan <thanzeelhassan@gmail.com>
2024-12-14 23:14:09 +0530

    Make an endpoint which shows a basic json response

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Welcome to SplitLite");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to SplitLite");
+});
+
+app.get("/name", (req, res) => {
+  res.json({ name: "John" });
+});
+
+app.listen(3000);
