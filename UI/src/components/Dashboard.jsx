3593bda99c8e045b61e9a9813af04331c2683f44
Kiran Pradeep <kiranpradeep499@gmail.com>
2025-01-09 23:06:07 +0530

    Navbar enhancements

--- UI/src/components/Dashboard.jsx
+++ UI/src/components/Dashboard.jsx
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from "react";
 import ToastContainerComponent from "./Toasts";
 import { toast } from "react-toastify";
-import "./dashboard.css"; // Import the CSS for styling
 
 const baseUrl = import.meta.env.VITE_API_URL;
 
 function Dashboard() {
+  const [activeNavItem, setActiveNavItem] = useState("profile");
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+
+  const activeNavItemStyling = {
+    backgroundColor: "#079a98",
+    color: "white",
+  };
 
   useEffect(() => {
     const fetchProtectedData = async () => {
@@ -32,7 +37,9 @@ function Dashboard() {
           setUser(data.user); // Set the user object with the decoded user data
         } else {
           const errorData = await response.json();
-          toast.error(errorData.message || "Access denied. Please log in again.");
+          toast.error(
+            errorData.message || "Access denied. Please log in again."
+          );
           setUser(null);
         }
       } catch (error) {
@@ -51,27 +58,33 @@ function Dashboard() {
     return <h1>Loading...</h1>;
   }
 
+  function handleNavItemClick(item) {
+    setActiveNavItem(item);
+  }
   return (
     <div>
       <ToastContainerComponent />
       {user ? (
-        <>
+        <div>
           <div className="nav-bar">
             <h1>SplitLite</h1>
             <ul>
-              <li>
-                <a href="/profile">Profile</a>
-              </li>
-              <li>
-                <a href="/groups">Groups</a>
-              </li>
-              <li>
-                <a href="/logout">Logout</a>
-              </li>
+              {["profile", "groups", "logout"].map((item) => (
+                <li>
+                  <a
+                    key={item}
+                    href={`#${item}`}
+                    onClick={() => handleNavItemClick(item)}
+                    style={activeNavItem === item ? activeNavItemStyling : {}}
+                  >
+                    {item.charAt(0).toUpperCase() + item.slice(1)}
+                  </a>
+                </li>
+              ))}
             </ul>
           </div>
-          <h3>Welcome to the dashboard, {user.name}!</h3>
-        </>
+          <h3 className="greeting">Welcome back, {user.name}!</h3>
+        </div>
       ) : (
         <h1>Access denied</h1>
       )}
