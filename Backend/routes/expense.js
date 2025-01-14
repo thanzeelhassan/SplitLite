8e44f17e2cdcdecd6d0c4fbc3c88f92177e59df4
Thanzeel Hassan <thanzeelhassan@gmail.com>
2025-01-14 15:28:23 +0530

    added endpoint to add settlement

--- Backend/routes/expense.js
+++ Backend/routes/expense.js
@@ -34,4 +34,31 @@ router.post('/expense', authenticateToken, async (req, res) => {
     }
 });
 
+// Add a new settlement
+router.post('/settlement', authenticateToken, async (req, res) => {
+    try {
+        const { group_id, payer_id, payee_id, amount } = req.body;
+
+        // Insert the group into the database
+        const result = await sql`
+      INSERT INTO settlements (group_id, payer_id, payee_id, amount, created_at)
+      VALUES (${group_id}, ${payer_id}, ${payee_id}, ${amount}, NOW())
+      RETURNING settlement_id, group_id, payer_id, payee_id, amount, created_at;
+    `;
+
+        if (result.length === 0) {
+            return res.status(500).send('Failed to add settlement.');
+        }
+
+        const group = result[0];
+        res.status(201).json({
+            message: 'Settlement added successfully.',
+            group,
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('An error occurred while adding the settlement.');
+    }
+});
+
 module.exports = router;
