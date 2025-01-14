e011691f302cc63ef90b24ca43ab63846591c543
Thanzeel Hassan <thanzeelhassan@gmail.com>
2025-01-14 15:28:41 +0530

    added endpoint to add group member

--- Backend/routes/group.js
+++ Backend/routes/group.js
@@ -66,4 +66,32 @@ router.get("/groups", authenticateToken, async (req, res) => {
   }
 });
 
+// Create a new group
+router.post("/groupmembers", authenticateToken, async (req, res) => {
+  try {
+    console.log(req.user);
+    const { group_id, user_id } = req.body;
+
+    // Insert the group into the database
+    const result = await sql`
+      INSERT INTO groupmembers (group_id, user_id)
+      VALUES (${group_id}, ${user_id})
+      RETURNING group_member_id, group_id, user_id, joined_at;
+    `;
+
+    if (result.length === 0) {
+      return res.status(500).send("Failed to add group member.");
+    }
+
+    const group = result[0];
+    res.status(201).json({
+      message: "Group member added successfully.",
+      group,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("An error occurred while adding the group member.");
+  }
+});
+
 module.exports = router;
