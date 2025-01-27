177e05b5ae3ff7f1f49aa4a10b67eedae9d441da
Thanzeel Hassan <thanzeelhassan@gmail.com>
2025-01-27 16:43:21 +0530

    make Members and Expenses seperate components

--- UI/src/components/GroupDetails.jsx
+++ UI/src/components/GroupDetails.jsx
@@ -3,6 +3,8 @@ import { motion } from "framer-motion";
 import GroupService from "./GroupService";
 import PendingPayments from "./GroupDetails/PendingPayments";
 import SettlementsList from "./GroupDetails/SettlementsList";
+import ExpensesList from "./GroupDetails/ExpensesList";
+import MembersList from "./GroupDetails/MembersList";
 
 function GroupDetails({ group, onBackClick }) {
   const [members, setMembers] = useState([]);
@@ -269,81 +271,10 @@ function GroupDetails({ group, onBackClick }) {
       </div>
 
       {/* Members section */}
-      <h2>Members</h2>
-      <ul
-        style={{
-          listStyle: "none",
-          padding: 0,
-        }}
-      >
-        {members.length > 0 ? (
-          members.map((member) => (
-            <li
-              key={member.id}
-              style={{
-                marginBottom: "10px",
-                padding: "15px",
-                border: "1px solid #ccc",
-                borderRadius: "8px",
-              }}
-            >
-              Name : {member.name}
-              <br />
-              Email : ({member.email})
-            </li>
-          ))
-        ) : (
-          <p>No members found.</p>
-        )}
-      </ul>
+      <MembersList members={members} />
 
       {/* Expenses section */}
-      <h2>Expenses</h2>
-      <ul style={{ listStyle: "none", padding: 0 }}>
-        {expenses.length > 0 ? (
-          expenses.map((expense) => (
-            <li
-              key={expense.expense_id}
-              style={{
-                marginBottom: "20px", // Add spacing between expenses
-                padding: "15px",
-                border: "1px solid #ccc",
-                borderRadius: "8px",
-              }}
-            >
-              <div style={{ marginBottom: "10px" }}>
-                {expense.amount} paid by {expense.name} | Description:{" "}
-                {expense.description}
-              </div>
-              <div>
-                <strong>Participants:</strong>
-                <ul style={{ paddingLeft: "20px", marginTop: "10px" }}>
-                  {participants[expense.expense_id] &&
-                  participants[expense.expense_id].length > 0 ? (
-                    participants[expense.expense_id].map((participant) => (
-                      <li
-                        key={participant.expense_participant_id}
-                        style={{
-                          marginBottom: "5px", // Add spacing between participants
-                        }}
-                      >
-                        {participant.participant_name} owes{" "}
-                        {participant.amount_owed} to {participant.paid_by_name}
-                      </li>
-                    ))
-                  ) : (
-                    <li style={{ color: "#777" }}>
-                      No participants found for this expense.
-                    </li>
-                  )}
-                </ul>
-              </div>
-            </li>
-          ))
-        ) : (
-          <p>No expenses found.</p>
-        )}
-      </ul>
+      <ExpensesList expenses={expenses} participants={participants} />
 
       {/* Settlements section */}
       <SettlementsList settlements={settlements} />
